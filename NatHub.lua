08c5918eba2134214628def3de2fdd307bd4c536
coolkidddddd <138830533+coolkidddddd@users.noreply.github.com>
2025-06-12 14:58:35 -0500

    Update NatHub.lua

--- NatHub.lua
+++ NatHub.lua
@@ -1,69 +1,39 @@
-local supportedGames = {}
-local scriptID = {}
-if premium then
-    supportedGames = {
-        [7436755782] = "https://api.luarmor.net/files/v4/loaders/483d639ad74a7814ff1057d68cec56c2.lua", -- Grow a Garden
-        [7018190066] = "https://api.luarmor.net/files/v4/loaders/d3a76114c1ea182127b88170b6043d11.lua", -- Dead Rails
-        [5750914919] = "https://raw.githubusercontent.com/ArdyBotzz/NatHub/refs/heads/master/fisch.lua", -- Fisch
-        [6325068386] = "https://api.luarmor.net/files/v4/loaders/a0ad31cf58a8bd98dd82fa1fb648290f.lua", -- BLR
-        [4777817887] = "https://api.luarmor.net/files/v4/loaders/d53370331c9ca16ce3479c3ac6ae5a78.lua", -- Bladeball
-        [7095682825] = "https://raw.githubusercontent.com/ArdyBotzz/NatHub/refs/heads/master/beaks.lua", -- Beaks
-        [994732206] = "https://raw.githubusercontent.com/ArdyBotzz/NatHub/refs/heads/master/bf.lua" -- Blox Fruit
-    }
-    scriptID = {
-        [7436755782] = "483d639ad74a7814ff1057d68cec56c2", -- Grow a Garden
-        [7018190066] = "d3a76114c1ea182127b88170b6043d11", -- Dead Rails
-        [6325068386] = "a0ad31cf58a8bd98dd82fa1fb648290f", -- BLR
-        [4777817887] = "d53370331c9ca16ce3479c3ac6ae5a78" -- BB
-    }
-else
-    supportedGames = {
-        [7436755782] = "https://api.luarmor.net/files/v4/loaders/65c66a87b33565a9dea1a54b798b6b2a.lua", -- Grow a Garden
-        [7018190066] = "https://api.luarmor.net/files/v4/loaders/a3e99a8c1a465fc973e7aa0dda0e220c.lua", -- Dead Rails
-        [5750914919] = "https://raw.githubusercontent.com/ArdyBotzz/NatHub/refs/heads/master/fisch.lua", -- Fisch
-        [6325068386] = "https://api.luarmor.net/files/v4/loaders/50ba70185011d66f3ed97e4e7f50bd11.lua", -- BLR
-        [4777817887] = "https://api.luarmor.net/files/v4/loaders/6f48a7a95292a0885256d242900d81fb.lua", -- Bladeball
-        [7095682825] = "https://raw.githubusercontent.com/ArdyBotzz/NatHub/refs/heads/master/beaks.lua", -- Beaks
-        [994732206] = "https://raw.githubusercontent.com/ArdyBotzz/NatHub/refs/heads/master/bf.lua" -- Blox Fruit
-    }
-    scriptID = {
-        --[7436755782] = "65c66a87b33565a9dea1a54b798b6b2a", -- Grow a Garden
-        [7018190066] = "a3e99a8c1a465fc973e7aa0dda0e220c", -- Dead Rails
-        [6325068386] = "50ba70185011d66f3ed97e4e7f50bd11", -- BLR
-        [4777817887] = "6f48a7a95292a0885256d242900d81fb" -- BB
-    }
-end
-local gameNames = {
-    [5750914919] = "Fisch", -- Fisch
-    [7018190066] = "Dead Rails", -- Dead Rails
-    [6325068386] = "Blue Lock Rivals", -- BLR
-    [4777817887] = "Blade Ball", -- Blade Ball
-    [7436755782] = "GaG", -- GaG
-    [7095682825] = "Beaks", -- Beaks
-    [994732206] = "Blox Fruit"
-}
-local gameId = game.GameId
-if supportedGames[gameId] then
-    game.StarterGui:SetCore(
-        "SendNotification",
-        {
-            Title = "NatHub Loaded!",
-            Text = gameNames[gameId] .. " Script Loaded!",
-            Icon = "rbxassetid://99764942615873",
-            Duration = 5
-        }
-    )
-    -- key system removed here
-    local game_url = game:HttpGet(supportedGames[gameId])
-    loadstring(game_url)()
-else
-    game.StarterGui:SetCore(
-        "SendNotification",
-        {
-            Title = "NatHub Error!",
-            Text = "You Execute Non Supported Game",
-            Icon = "rbxassetid://99764942615873",
-            Duration = 5
-        }
-    )
-end
+if game.PlaceId == 109983668079237 then
+
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+
+local Window = Rayfield:CreateWindow({
+   Name = "Steal A Brainrot Cheats",
+   Icon = 0, -- Icon in Topbar. Can use Lucide Icons (string) or Roblox Image (number). 0 to use no icon (default).
+   LoadingTitle = "Example Hub",
+   LoadingSubtitle = "by [NSM] Coolkid",
+   Theme = "Amethyst", -- Check https://docs.sirius.menu/rayfield/configuration/themes
+
+   ToggleUIKeybind = "K", -- The keybind to toggle the UI visibility (string like "K" or Enum.KeyCode)
+
+   DisableRayfieldPrompts = false,
+   DisableBuildWarnings = false, -- Prevents Rayfield from warning when the script has a version mismatch with the interface
+
+   ConfigurationSaving = {
+      Enabled = true,
+      FolderName = nil, -- Create a custom folder for your hub/game
+      FileName = "Part"
+   },
+
+   Discord = {
+      Enabled = false, -- Prompt the user to join your Discord server if their executor supports it
+      Invite = "noinvitelink", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ ABCD would be ABCD
+      RememberJoins = true -- Set this to false to make them join the discord every time they load it up
+   },
+
+   KeySystem = true, -- Set this to true to use our key system
+   KeySettings = {
+      Title = "Steal A Brainrot | Key",
+      Subtitle = "You Aint gettin shit nigga 😂😂😂",
+      Note = "So like A nigger to the nig", -- Use this to tell the user how to get a key
+      FileName = "SkillaMayKillANigga", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
+      SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
+      GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
+      Key = {"FUCKFAGGOTNIGGERSAllshoulddie@@@%%%!%#$%^&*("} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
+   }
+})
